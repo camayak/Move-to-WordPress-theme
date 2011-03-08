6c22a64c153f25c23ab8793fc3cacfce9940ab90
Daniel Bachhuber <d@danielbachhuber.com>
2011-03-08 10:40:34 +0800

    More base functionality

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+
+define( 'MOVETOWORDPRESS_VERSION', '0.0' );
+
+if ( !class_exists( 'movetowordpress' ) ) {
+	
+class movetowordpress
+{
+
+	/**
+	 * __construct()
+	 */
+	function __construct() {
+		
+		add_action( 'init', array( &$this, 'enqueue_styles' ) );
+		
+	} // END __construct()
+	
+	/**
+	 * enqueue_styles()
+	 */
+	function enqueue_styles() {
+		
+		if ( !is_admin() ) { 
+			wp_enqueue_style( 'movetowordpress', get_bloginfo( 'template_directory' ) . '/style.css', false, MOVETOWORDPRESS_VERSION );
+		}
+		
+	} // END enqueue_styles()
+	
+} // END class movetowordpress
+	
+} // END if ( !class_exists( 'movetowordpress' ) )
+
+$towknightcenter = new towknightcenter();
+
+
+
+
+
+
+?>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+	
+	<title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	
+	<?php wp_head(); ?>
+	
+</head>
+<body <?php body_class(); ?>>
+
+<div class="header">
+	
+	<div class="wrap">
+		
+		
+	</div>
+	
+</div><!-- END .header -->
